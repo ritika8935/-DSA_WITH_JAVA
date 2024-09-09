4d16a561de59319e77fa15bbea11be0b51f69264
Ritika Singh <singhritika8935@gmail.com>
2024-09-10 00:46:35 +0530

    math for dsa

---
+++ src/MATH_DSA/bitwise.java
@@ -0,0 +1,11 @@
+package MATH_DSA;
+
+public class bitwise {
+    public static void main(String[] args) {
+        int n=68;
+        System.out.println(isodd(n));
+    }
+    static boolean isodd(int n){
+        return (n & 1)==1;
+    }
+}
